2d16d1ed16e058dda42fd501d8b603eed59006c0
daniel-ljung-nti-johanneberg <daniel.ljung@elev.ga.ntig.se>
2022-05-11 10:35:39 +0200

    Revert "Formatting"
    This reverts commit e1b01c754cd6818a3ac4ec0cef227f7f096d0245.

--- functions.rb
+++ functions.rb
@@ -1,38 +1,50 @@
 def current_user
-  User.from_id(session[:user_id])
+    User.from_id(session[:user_id])
 end
+
 
 def current_items(user_id)
 
-  # Get Items
+    # Get Items
 
-  #UserItemRelation = User.from_id(session[:user_id])
+    #UserItemRelation = User.from_id(session[:user_id])
 
-  if !user == nil
-  end
+    if !user == nil
+    end
+
 end
 
 def format_trades(trades)
-  trades.map! do |trade|
-    new_trade = Array.new()
-    trade_users = Array.new()
 
-    new_trade << trade["id"]
+    trades.map! do |trade|
+    
+        new_trade = Array.new()
+        trade_users = Array.new()
 
-    new_trade << JSON[trade["sender"]]
-    trade_users << new_trade.last.pop[1..-1].to_i
+        new_trade << trade["id"]
 
-    new_trade << JSON[trade["reciever"]]
-    trade_users << new_trade.last.pop[1..-1].to_i
+        new_trade << JSON[trade["sender"]]
+        trade_users << new_trade.last.pop[1..-1].to_i
 
-    new_trade[1].map! do |id|
-      Item.from_id(id)
+        new_trade << JSON[trade["reciever"]]
+        trade_users << new_trade.last.pop[1..-1].to_i
+
+        new_trade[1].map! do |id|
+
+            Item.from_id(id)
+                 
+        end
+
+        new_trade[2].map! do |id|
+
+            Item.from_id(id)
+
+        end
+
+        new_trade << trade_users
+
     end
 
-    new_trade[2].map! do |id|
-      Item.from_id(id)
-    end
-
-    new_trade << trade_users
-  end
+  
 end
+
--- model.rb
+++ model.rb
@@ -1,243 +1,337 @@
 def db
-  _db = SQLite3::Database.new("data/database.db")
-  _db.results_as_hash = true
-  _db
+    _db = SQLite3::Database.new("data/database.db")
+    _db.results_as_hash = true
+    _db
 end
 
+
 class BaseModel
-  attr_reader :id
 
-  def self.table; end
+    attr_reader :id
 
-  def initialize(data)
-    @id = data["id"]
-  end
+    def self.table; end
 
-  def self.from_id(id)
-    result = db.execute("SELECT * FROM #{self.table} WHERE id = ?", id).first
+    def initialize(data)
 
-    result && new(result) # Om result finns skapa ett nytt objekt
-  end
+        @id = data['id']
+
+
+    end
+
+    def self.from_id(id)
+
+        result = db.execute("SELECT * FROM #{self.table} WHERE id = ?", id).first
+        
+        result && new(result) # Om result finns skapa ett nytt objekt
+
+    end
+
+
 end
 
 class User < BaseModel
-  attr_reader :username, :coins, :rank, :password_hash
 
-  def self.table
-    "User"
-  end
+    attr_reader :username, :coins, :rank, :password_hash
 
-  def initialize(data)
-    super data
+    def self.table
 
-    @username = data["username"]
-    @coins = data["coins"]
-    @rank = data["rank"]
-    @password_hash = data["password"]
-  end
+        'User'
 
-  def self.from_username(username)
-    result = db.execute("SELECT * FROM User WHERE username = ?", [username]).first
-
-    result && new(result)
-  end
-
-  def self.select_id(username)
-    return db.execute("SELECT id FROM User WHERE username=?", username)
-  end
-
-  def self.create(user, pwd_digest)
-    db.execute("INSERT INTO User (username, password) VALUES(?, ?)", user, pwd_digest)
-  end
-
-  def self.setcoins(coins, userid)
-    db.execute("UPDATE User SET coins = ? WHERE id = ? ", coins, userid)
-  end
-
-  def self.create_password(pwd)
-    return BCrypt::Password.create(pwd)
-  end
-
-  def self.check_password(password_hash, pwd)
-    return BCrypt::Password.new(password_hash) == pwd
-  end
-
-  def self.search(query)
-    users = db.execute("SELECT * FROM User WHERE username LIKE '%#{query}%'")
-
-    users.map do |user|
-      new user
-    end
-  end
-
-  def self.LoadItems(userid)
-
-    # If userid unspecified, load all store items
-
-    if !userid
-      return db.execute("SELECT * FROM Items")
     end
 
-    their_items = Array.new
+    def initialize(data)
 
-    relationtable = db.execute("SELECT * FROM UserItemRelation")
+        super data
 
-    relationtable.each do |element|
-      if element["userid"] == userid
-        their_items << element["itemid"]
-      end
+        @username = data['username']
+        @coins = data['coins']
+        @rank = data['rank']
+        @password_hash = data['password']
+
     end
 
-    return their_items
-  end
+    def self.from_username(username)
 
-  def self.recieve_item(user_id, item_id)
-    db.execute("INSERT into UserItemRelation (userid, itemid) VALUES (?, ?)", user_id, item_id)
-  end
+        result = db.execute("SELECT * FROM User WHERE username = ?", [username]).first
+
+        result && new(result)
+
+    end
+
+    def self.select_id(username)
+
+        return db.execute('SELECT id FROM User WHERE username=?',username)
+
+    end
+
+    def self.create(user, pwd_digest)
+
+        db.execute('INSERT INTO User (username, password) VALUES(?, ?)', user, pwd_digest)
+
+    end
+
+    def self.setcoins(coins, userid)
+
+        db.execute("UPDATE User SET coins = ? WHERE id = ? ",coins,userid)
+
+    end
+
+    def self.create_password(pwd)
+
+        return BCrypt::Password.create(pwd)
+
+    end
+
+    def self.check_password(password_hash, pwd)
+
+        return BCrypt::Password.new(password_hash) == pwd
+
+    end
+
+
+
+    def self.search(query)
+
+
+        users = db.execute("SELECT * FROM User WHERE username LIKE '%#{query}%'")
+
+        users.map do |user|
+
+            new user
+
+        end
+
+
+    end
+
+    
+    def self.LoadItems(userid)
+
+
+        # If userid unspecified, load all store items
+
+        if !userid
+            return db.execute("SELECT * FROM Items")
+        end
+
+        their_items = Array.new
+
+        relationtable = db.execute("SELECT * FROM UserItemRelation")
+
+        relationtable.each do |element|
+
+            if element["userid"] == userid
+    
+                their_items << element["itemid"] 
+    
+            end
+
+        end
+
+        return their_items
+ 
+    end
+
+    def self.recieve_item(user_id, item_id)
+
+        db.execute("INSERT into UserItemRelation (userid, itemid) VALUES (?, ?)", user_id, item_id)
+
+    end
+
+
 end
 
+
+
 class Item < BaseModel
-  attr_reader :name, :price, :image_url, :id, :stock
 
-  def self.table
-    "Items"
-  end
+    attr_reader :name, :price, :image_url, :id, :stock
 
-  def initialize(data)
-    super data
-    @image_url = data["image_url"]
-    @price = data["price"]
-    @name = data["name"]
-    @id = data["id"]
-  end
+    def self.table
 
-  def self.create(name, price, image_url)
-    db.execute("INSERT into Items (name, price, image_url) VALUES (?, ?, ?)", name, price, image_url)
-  end
+        'Items'
 
-  def self.item_name(item_name)
-    db.execute("SELECT name FROM Items WHERE name = ?", item_name)
-  end
+    end
+ 
+    def initialize(data)
+        super data
+        @image_url = data["image_url"]
+        @price = data["price"]
+        @name = data["name"]
+        @id = data["id"]
 
-  def self.item_id(item_name)
-    db.execute("SELECT id FROM Items WHERE name = ?", item_name).first["id"]
-  end
+    end
 
-  def self.delete(item_name, item_id)
-    db.execute("DELETE FROM Items WHERE name = ?", item_name)
+    def self.create(name, price, image_url)
 
-    db.execute("DELETE FROM UserItemRelation WHERE itemid = ?", item_id)
-  end
+        db.execute("INSERT into Items (name, price, image_url) VALUES (?, ?, ?)", name, price, image_url)
 
-  def self.price(item_id)
-    db.execute("SELECT price FROM Items WHERE id = ?", [item_id]).first["price"]
-  end
+    end
+
+    def self.item_name(item_name)
+
+        db.execute("SELECT name FROM Items WHERE name = ?", item_name)
+
+    end
+
+    def self.item_id(item_name)
+
+        db.execute("SELECT id FROM Items WHERE name = ?", item_name).first["id"]
+
+    end
+
+    def self.delete(item_name, item_id)
+
+        db.execute("DELETE FROM Items WHERE name = ?", item_name)
+
+        db.execute("DELETE FROM UserItemRelation WHERE itemid = ?", item_id)
+
+    end
+
+    def self.price(item_id)
+
+        db.execute("SELECT price FROM Items WHERE id = ?", [item_id]).first["price"]
+
+    end
+
 end
 
 class Trades < BaseModel
-  def self.table
-    "Trades"
-  end
 
-  def initialize(data)
-    super data
-    @id = data["id"]
-    @sender = data["price"]
-    @reciever = data["name"]
-  end
 
-  def self.list(userid)
-    trades = db.execute("SELECT * FROM Trades WHERE reciever LIKE '%U#{userid}%'")
-    new_trades = format_trades(trades)
-  end
+    def self.table
 
-  def self.select(tradeid)
-    trade = db.execute("SELECT * FROM Trades WHERE id = ?", tradeid).first
-  end
+        'Trades'
 
-  def self.exchange(trade, tradeid, reciever)
-    match1 = true
-    match2 = true
-    trade = Trades.select(tradeid)
-    new_trade = Array.new()
-    trade_users = Array.new()
+    end
+ 
+    def initialize(data)
+     
+        super data
+        @id = data["id"]
+        @sender = data["price"]
+        @reciever = data["name"]
 
-    p trade
-
-    if trade == nil
-      return
     end
 
-    new_trade << JSON[trade["sender"]]
-    trade_users << new_trade.last.pop[1..-1].to_i
+    def self.list(userid)
 
-    new_trade << JSON[trade["reciever"]]
-    trade_users << new_trade.last.pop[1..-1].to_i
+        trades = db.execute("SELECT * FROM Trades WHERE reciever LIKE '%U#{userid}%'")
+        new_trades = format_trades(trades)
 
-    if reciever != trade_users[1]
-      return false
     end
 
-    for itemid in new_trade.first
-      rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, trade_users[0])
-      if rowid.length < 1
-        match1 = false
-      end
+    def self.select(tradeid)
+
+        trade = db.execute("SELECT * FROM Trades WHERE id = ?", tradeid).first
+
     end
 
-    for itemid in new_trade.last
-      rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, reciever)
-      if rowid.length < 1
-        match2 = false
-      end
+    def self.exchange(trade, tradeid, reciever)
+
+        match1 = true
+        match2 = true
+        trade = Trades.select(tradeid)
+        new_trade = Array.new()
+        trade_users = Array.new()
+
+        p trade
+
+        if trade == nil
+            return
+        end
+    
+        new_trade << JSON[trade["sender"]]
+        trade_users << new_trade.last.pop[1..-1].to_i
+    
+        new_trade << JSON[trade["reciever"]]
+        trade_users << new_trade.last.pop[1..-1].to_i
+    
+        if reciever != trade_users[1]
+    
+            return false
+
+        end
+      
+        for itemid in new_trade.first
+           
+            rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, trade_users[0])
+            if rowid.length < 1
+                match1 = false
+            end
+    
+        end
+    
+        for itemid in new_trade.last
+    
+            rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, reciever)
+            if rowid.length < 1
+                match2 = false
+            end
+    
+        end
+    
+        if match1 && match2 == true
+    
+            for itemid in new_trade.first
+                rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, trade_users[0])
+                new_rowid = rowid.first
+                db.execute("DELETE FROM UserItemRelation WHERE rowid = ?", new_rowid["rowid"])
+    
+            end
+    
+            for itemid in new_trade.last
+                rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, reciever)
+                new_rowid = rowid.first
+                db.execute("DELETE FROM UserItemRelation WHERE rowid = ?", new_rowid["rowid"])
+            end
+    
+            for itemid in new_trade[0]
+    
+                db.execute('INSERT into UserItemRelation (userid, itemid) VALUES (?, ?)', reciever, itemid)
+    
+            end
+    
+            for itemid in new_trade[1]
+    
+                db.execute('INSERT into UserItemRelation (userid, itemid) VALUES (?, ?)', trade_users[0], itemid)
+    
+            end
+
+            return true
+    
+        end
+
+        return false
+        
+
     end
 
-    if match1 && match2 == true
-      for itemid in new_trade.first
-        rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, trade_users[0])
-        new_rowid = rowid.first
-        db.execute("DELETE FROM UserItemRelation WHERE rowid = ?", new_rowid["rowid"])
-      end
+    def self.create(from_items, to_items)
 
-      for itemid in new_trade.last
-        rowid = db.execute("SELECT rowid, * FROM UserItemRelation WHERE itemid = ? AND userid = ?", itemid, reciever)
-        new_rowid = rowid.first
-        db.execute("DELETE FROM UserItemRelation WHERE rowid = ?", new_rowid["rowid"])
-      end
+        db.execute('INSERT into Trades (sender, reciever) VALUES (?, ?)', from_items, to_items)
 
-      for itemid in new_trade[0]
-        db.execute("INSERT into UserItemRelation (userid, itemid) VALUES (?, ?)", reciever, itemid)
-      end
-
-      for itemid in new_trade[1]
-        db.execute("INSERT into UserItemRelation (userid, itemid) VALUES (?, ?)", trade_users[0], itemid)
-      end
-
-      return true
     end
 
-    return false
-  end
+    def self.delete(tradeid, user)
 
-  def self.create(from_items, to_items)
-    db.execute("INSERT into Trades (sender, reciever) VALUES (?, ?)", from_items, to_items)
-  end
+        trade = select(tradeid)
+        new_trade = Array.new()
+        trade_users = Array.new
 
-  def self.delete(tradeid, user)
-    trade = select(tradeid)
-    new_trade = Array.new()
-    trade_users = Array.new
+        new_trade << JSON[trade["sender"]]
+        trade_users << new_trade.last.pop[1..-1].to_i
+    
+        new_trade << JSON[trade["reciever"]]
+        trade_users << new_trade.last.pop[1..-1].to_i
 
-    new_trade << JSON[trade["sender"]]
-    trade_users << new_trade.last.pop[1..-1].to_i
+        reciever = trade_users[1]
 
-    new_trade << JSON[trade["reciever"]]
-    trade_users << new_trade.last.pop[1..-1].to_i
+        if reciever == user
+            db.execute("DELETE FROM Trades WHERE id = ?", tradeid)
+        end
 
-    reciever = trade_users[1]
-
-    if reciever == user
-      db.execute("DELETE FROM Trades WHERE id = ?", tradeid)
     end
-  end
+
+
 end
